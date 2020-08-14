be77a91f57e653b570cf2ea3dbf059a57d81ecf3
StarWolf19950320 <starwolf0320@gmail.com>
2020-08-14 10:05:24 +0430

    Video-04-Product-Details-Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,40 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap';
-import { prodcuts } from './products';
-import Product from './components/Product';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div>
-      <header>
-        <Navbar expand="lg" bg="dark" variant="dark" collapseOnSelect>
-          <Navbar.Brand href="/">MERN Marketplace</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbar-menu"></Navbar.Toggle>
-          <Navbar.Collapse id="navbar-menu" className="justify-content-end">
-            <Nav>
-              <Nav.Link href="/cart">Cart</Nav.Link>
-              <Nav.Link href="/signin">Sign In</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </header>
-      <main>
-        <Container fluid>
-          <h1>Featured Products</h1>
-          <Row>
-            {prodcuts.map((product) => (
-              <Col key={product._id} sm={12} md={6} lg={3}>
-                <Product product={product}></Product>
-              </Col>
-            ))}
-          </Row>
-        </Container>
-      </main>
-      <footer>
-        <Container>
-          <Row>
-            <Col className="text-center py-3">@2020 All right reserved.</Col>
-          </Row>
-        </Container>
-      </footer>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Navbar expand="lg" bg="dark" variant="dark" collapseOnSelect>
+            <Navbar.Brand href="/">MERN Marketplace</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar-menu"></Navbar.Toggle>
+            <Navbar.Collapse id="navbar-menu" className="justify-content-end">
+              <Nav>
+                <Nav.Link href="/cart">Cart</Nav.Link>
+                <Nav.Link href="/signin">Sign In</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </header>
+        <main>
+          <Container fluid>
+            <Route path="/" component={HomeScreen} exact></Route>
+            <Route path="/product/:id" component={ProductScreen} exact></Route>
+          </Container>
+        </main>
+        <footer>
+          <Container>
+            <Row>
+              <Col className="text-center py-3">@2020 All right reserved.</Col>
+            </Row>
+          </Container>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
