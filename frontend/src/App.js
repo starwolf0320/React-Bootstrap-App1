dcaa716bf1b0253d8f15e0c495051a9ac243225d
StarWolf19950320 <starwolf0320@gmail.com>
2020-08-14 11:18:37 +0430

    Video-04-Produc-Details-Screen-Fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
@@ -10,20 +11,26 @@ function App() {
       <div>
         <header>
           <Navbar expand="lg" bg="dark" variant="dark" collapseOnSelect>
-            <Navbar.Brand href="/">MERN Marketplace</Navbar.Brand>
+            <LinkContainer to="/">
+              <Navbar.Brand>MERN Marketplace</Navbar.Brand>
+            </LinkContainer>
             <Navbar.Toggle aria-controls="navbar-menu"></Navbar.Toggle>
             <Navbar.Collapse id="navbar-menu" className="justify-content-end">
               <Nav>
-                <Nav.Link href="/cart">Cart</Nav.Link>
-                <Nav.Link href="/signin">Sign In</Nav.Link>
+                <LinkContainer to="/cart">
+                  <Nav.Link>Cart</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/signin">
+                  <Nav.Link>Sign In</Nav.Link>
+                </LinkContainer>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
         </header>
         <main>
           <Container fluid>
+            <Route path="/product/:id" component={ProductScreen}></Route>
             <Route path="/" component={HomeScreen} exact></Route>
-            <Route path="/product/:id" component={ProductScreen} exact></Route>
           </Container>
         </main>
         <footer>
