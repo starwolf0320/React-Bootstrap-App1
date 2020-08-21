25ec9e81fb337efa2aae67c566d526534d566ba7
StarWolf19950320 <starwolf0320@gmail.com>
2020-08-21 12:19:57 +0430

    Video-06-Load-Products-From-Backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import { prodcuts } from '../products';
+import axios from 'axios';
+// import { prodcuts } from '../products';
 
 export default function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      // send ajax request
+      const { data } = await axios.get(`/api/products`);
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
   return (
     <>
       <h1>Featured Products</h1>
       <Row>
-        {prodcuts.map((product) => (
+        {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={3}>
             <Product product={product}></Product>
           </Col>
