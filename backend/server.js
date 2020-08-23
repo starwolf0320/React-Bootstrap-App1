a5de43c186fab2292f71ffb126ae674aae5e1fe2
StarWolf19950320 <starwolf0320@gmail.com>
2020-08-23 11:41:20 +0430

    Video-11-Add-Redux-To-Product-Screen

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
+/* eslint-disable no-underscore-dangle */
 import express from 'express';
 import products from './products';
 
 const app = express();
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
 app.get('/api/products', (req, res) => {
   // res.status(500).send({ message: 'Can not get products' });
   res.send(products);
