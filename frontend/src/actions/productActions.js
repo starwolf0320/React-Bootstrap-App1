4be69870c4870ab77619a840dbf4792215f92b90
StarWolf19950320 <starwolf0320@gmail.com>
2020-09-10 09:19:11 +0430

    Video-39-Delete-Products

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -6,6 +6,9 @@ import {
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAIL,
+  PRODUCT_DELETE_FAIL,
+  PRODUCT_DELETE_REQUEST,
+  PRODUCT_DELETE_SUCCESS,
 } from '../constants/productConstants';
 
 export const listProducts = () => async (dispatch) => {
@@ -23,7 +26,28 @@ export const listProducts = () => async (dispatch) => {
     });
   }
 };
-
+export const deleteProduct = (productId) => async (dispatch, getState) => {
+  try {
+    const {
+      userSignin: { userInfo },
+    } = getState();
+    dispatch({ type: PRODUCT_DELETE_REQUEST, payload: productId });
+    const { data } = await Axios.delete(`/api/products/${productId}`, {
+      headers: {
+        Authorization: `bearer ${userInfo.token}`,
+      },
+    });
+    dispatch({ type: PRODUCT_DELETE_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DELETE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 export const detailsProduct = (productId) => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_DETAILS_REQUEST });
